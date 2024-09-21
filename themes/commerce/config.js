b745dc469e174a891fe8cee90727b1e4e08abf59
Xu hankun <xiuxiuxiu1357@gmail.com>
2024-09-21 22:44:59 +0800

    init

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -2,13 +2,13 @@ const CONFIG = {
   // 封面大图
   COMMERCE_HOME_BANNER_ENABLE: true,
 
-  COMMERCE_TEXT_CENTER_TITLE: 'Product Center', // 中间产品区块标题
+  COMMERCE_TEXT_CENTER_TITLE: '产品信息', // 中间产品区块标题
   COMMERCE_TEXT_CENTER_DESCRIPTION:
-    'The vision of NotionNext is to help you effortlessly and seamlessly build your own website, amplifying the value of your brand.', // 中间产品区块文字描述
-  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: 'Product Categories', // 左侧产品分类标题
-  COMMERCE_TEXT_FOOTER_TITLE: 'Contact US', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
-  COMMERCE_TEXT_FOOTER_MENU_1: 'Product Center', // COMMERCE主题页脚左侧菜单标题1
-  COMMERCE_TEXT_FOOTER_MENU_2: 'About US', // COMMERCE主题页脚左侧菜单标题2
+    '这是当下最受欢迎的产品。当然，你也可以打造你自己的独特风格！', // 中间产品区块文字描述
+  COMMERCE_TEXT_CENTER_CATEGORY_TITLE: '产品分类', // 左侧产品分类标题
+  COMMERCE_TEXT_FOOTER_TITLE: '联系我们', // COMMERCE主题页脚文案标题；按Shift+Enter键可以换行
+  COMMERCE_TEXT_FOOTER_MENU_1: '产品中心', // COMMERCE主题页脚左侧菜单标题1
+  COMMERCE_TEXT_FOOTER_MENU_2: '关于我们', // COMMERCE主题页脚左侧菜单标题2
 
   COMMERCE_FOOTER_RIGHT_IMG_URL: null, // 显示页脚右侧的图片，通常放二维码
   COMMERCE_FOOTER_RIGHT_TEXT: null, // 页脚右侧图片下的文字描述
