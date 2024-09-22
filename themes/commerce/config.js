ae4aea5081a8e94540cf48a086d880a6e3d8851f
Xu hankun <xiuxiuxiu1357@gmail.com>
2024-09-22 09:02:01 +0800

    init

--- themes/commerce/config.js
+++ themes/commerce/config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   COMMERCE_FOOTER_RIGHT_IMG_URL: null, // 显示页脚右侧的图片，通常放二维码
   COMMERCE_FOOTER_RIGHT_TEXT: null, // 页脚右侧图片下的文字描述
 
-  COMMERCE_HOME_POSTS_COUNT: 9, // 首页展示商品数
+  COMMERCE_HOME_POSTS_COUNT: 6, // 首页展示商品数
   COMMERCE_CONTACT_WHATSAPP_SHOW: true, // 是否展示whatsapp联系按钮 请配置 CONTACT_WHATSAPP
   COMMERCE_CONTACT_TELEGRAM_SHOW: true // 联系栏展示telegram按钮 请配置 CONTACT_TELEGRAM
 }
